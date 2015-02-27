c44995fa47ddfe86cb2253b34f8745164080ad30
Harman Kumar <deskar.cse.iitd.ac.in>
2015-02-27 16:54:52 +0530

    LOL

--- server/server/download.cpp
+++ server/server/download.cpp
@@ -11,8 +11,15 @@ bool Server::handleDownload() {
     //I'm told that the file name will be sent to me.
     std::string cont(CONTINUE);
     conn.writeToSocket(cont);
+
     std::string file = "";
-    return conn.writeToSocket(file); //Gets the file to be downloaded.
+    return conn.readFromSocket(file); //Gets the file to be downloaded.
+
+    ifstream f;
+    string filepath =SERVER_DIRECTORY + user.userID + "/" + file ;
+    fstream reader;
+    reader.open(filepath.c_str(),ios::in);
+    conn.writeToSocket_file(reader);
 }
 
 #endif
